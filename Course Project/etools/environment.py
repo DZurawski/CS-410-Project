63d1537145073b91f8aa9f8aef07f9588c4ba56f
DZurawski <danrzurawski@gmail.com>
2018-12-14 23:18:10 -0600

    Added etools package

---
+++ Course Project/etools/environment.py
@@ -0,0 +1,12 @@
+import spacy
+import pickle
+
+# Spacy Language object. Used to create Document pipelines from strings.
+# The "en_core_web_md" model is a convolutional neural network trained on
+# English web blogs, comments and news stories.
+NLP = spacy.load("en_core_web_md")
+
+# Trained TF-IDF vectorizer from scikit-learn. Trained on Enron corpus e-mails.
+# 25,000 uniform randomly selected e-mails from entire bank of Enron corpus.
+with open("etools/tfidf.pkl", "rb") as file:
+    TFIDF = pickle.load(file)
---
+++ Course Project/etools/ranker.py
@@ -0,0 +1,74 @@
+import spacy
+import numpy as np
+import pandas as pd
+from sklearn.feature_extraction.stop_words import ENGLISH_STOP_WORDS
+from etools.environment import *
+
+train = pd.read_csv("etools/train.csv")
+
+def clean(document: str) -> str:
+    """Prepare a document for input into a text mining process.
+
+    Args:
+        document: An e-mail represented as a single string object.
+        
+    Returns:
+      A cleaned string. All characters that are not alphabetic or are
+      considered as whitespace are removed. All alphabetic characters are set
+      to lowercase. All words that are considered as stop words by Scikit-learn
+      are removed.
+    """
+    document = document.lower()
+    words = "".join(c for c in document if c.isalpha() or c.isspace()).split()
+    return " ".join(w for w in words if w not in ENGLISH_STOP_WORDS)
+
+# The contents of a set of important e-mails.    
+important = [
+    NLP(clean(doc))
+    for doc
+    in train[train["Important"] == 1]["Content"]
+]
+
+# The contents of a set of unimportant e-mails.
+unimportant = [
+    NLP(clean(doc))
+    for doc
+    in train[train["Important"] == 0]["Content"]
+]
+
+
+def score(document: str, train: pd.DataFrame = None) -> float:
+    """Assign an importance score to this document between 0 and 1.
+    
+    Args:
+        document: An e-mail represented as a single string object.
+        train: A DataFrame consisting of important e-mails. Defaults to the
+            preloaded *important* set if this variable is unspecified.
+        
+    Returns:
+        A float value between 0 and 1. Documents that score close to 0 are very
+        unimportant. Documents that score close to 1 are highly important.
+        Essentially, this scoring metric outputs the average semantic
+        similarity between the inputted document and the training set *train*
+        of important documents.
+    """
+    train = important if train is None else train
+    nlp   = NLP(document)
+    return np.mean([nlp.similarity(doc) for doc in train])
+    
+
+def is_important(document: str) -> bool:
+    """Return True if this document is important. Return False otherwise.
+    
+    Args:
+        document: An e-mail represented as a single string object.
+        
+    Returns:
+        A boolean value. True if this document is important. False otherwise.
+        Essentially, this function takes the semantic similarity between this
+        document and the set of important documents and then compares that
+        value with the semantic similarity between this document and the set of
+        unimportant documents. If the document is more similar to the important
+        e-mails, then the document is important. False otherwise.
+    """
+    return score(document, important) > score(document, unimportant)
