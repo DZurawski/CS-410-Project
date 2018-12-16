c3ee2b84c138bff496e7b1cce1c8761473071d8d
DZurawski <danrzurawski@gmail.com>
2018-12-15 19:10:18 -0600

    Fixed ranker function to output more interesting results

--- Course Project/etools/environment.py
+++ Course Project/etools/environment.py
@@ -4,7 +4,7 @@ import pickle
 # Spacy Language object. Used to create Document pipelines from strings.
 # The "en_core_web_md" model is a convolutional neural network trained on
 # English web blogs, comments and news stories.
-NLP = spacy.load("en_core_web_md")
+NLP = spacy.load("en_core_web_lg")
 
 # Trained TF-IDF vectorizer from scikit-learn. Trained on Enron corpus e-mails.
 # 25,000 uniform randomly selected e-mails from entire bank of Enron corpus.
